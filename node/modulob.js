f33c30383dc7e9f488903ba8428d924b1e744c24
carlos augusto <138498910+carlos04js@users.noreply.github.com>
2024-02-28 23:29:11 -0300

    Add files via upload

---
+++ node/modulob.js
@@ -0,0 +1,12 @@
+
+export const bomDia = 'Bom dia'
+export function boaNoite() {
+
+    return 'Boa noite'
+
+}
+
+console.log(bomDia),
+console.log(boaNoite())
+
+
---
+++ node/visãoGeral.js
@@ -0,0 +1,11 @@
+//NODE.JS
+
+//JS, LINGUAGEM QUE PERMITE TANTO RODAS NO BROWSER QUANTO COMO NO SERVIDOR
+
+//NODE.JS= É O AMBIENTE DE EXECUÇÃO DE JS, NO LADO DO SERVIDOR!
+
+//RUNTIME:Um sistema de tempo de execução, também chamado de ambiente de tempo de execução, implementa principalmente partes de um modelo de execução. Isso não deve ser confundido com a fase do ciclo de vida de tempo de execução de um programa, durante o qual o sistema de tempo de execução está em operação.
+
+//Em informática, tempo de execução ou runtime (termo em inglês), é o período em que um programa de computador permanece em execução.
+
+//NODE.JS É UM RUNTIME DE JS
